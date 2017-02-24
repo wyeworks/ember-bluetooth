85207aec71956724ecbfb9dd33ec469d77c00c57
Federico Kauffman <federico@wyeworks.com>
2017-02-24 15:17:28 -0300

    Add basic read capability

--- addon/services/bluetooth.js
+++ addon/services/bluetooth.js
@@ -1,7 +1,49 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  serviceName: null,
+  characteristicName: null,
+  characteristicValue: null,
+
   isAvailable() {
     return 'bluetooth' in navigator;
+  },
+
+  requestDevice() {
+    return this;
+  },
+
+  getService(serviceName) {
+    this.set('serviceName', serviceName);
+
+    return this;
+  },
+
+  getCharacteristic(characteristicName) {
+    this.set('characteristicName', characteristicName);
+
+    return this;
+  },
+
+  connect() {
+
+    if (this.isAvailable()) {
+      let serviceName = this.get('serviceName');
+      let characteristicName = this.get('characteristicName');
+      let options = { filters: [{ services: [serviceName] }] };
+
+      navigator.bluetooth.requestDevice(options)
+      .then(device => device.gatt.connect())
+      .then(server => server.getPrimaryService(serviceName))
+      .then(service => service.getCharacteristic(characteristicName))
+      .then(characteristic => {
+        return characteristic.readValue();
+      })
+      .then(value => {
+        console.log(`Battery level is: ${value.getUint8(0)}`);
+      })
+      .catch(error => { console.log(error); });
+    }
+
   }
 });
